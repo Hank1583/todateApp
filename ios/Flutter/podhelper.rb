bd64d1993a4aacc2d9e3bfcd45f4be62c4ec1512
Hank1583 <jouchanghung@gmail.com>
2025-08-26 15:31:13 +0800

    2025-08-26 upload 4-16

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,9 +1,22 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'pathname'
+
+def flutter_additional_ios_build_settings(installer)
+  installer.pods_project.targets.each do |t|
+    t.build_configurations.each do |c|
+      c.build_settings['ENABLE_BITCODE'] = 'NO'
+    end
+  end
+end
+
+# 內部：實際安裝 plugins（會搜尋 ios/、darwin/、root，並優先用 :podspec）
 def flutter_install_ios_plugin_pods
   plugins_json = File.expand_path(File.join(__dir__, '../../.flutter-plugins-dependencies'))
   raise "Missing #{plugins_json}. Run `flutter pub get`." unless File.exist?(plugins_json)
 
   data = JSON.parse(File.read(plugins_json))
-  ios_plugins = (data.dig('plugins','ios') || [])
+  ios_plugins  = (data.dig('plugins','ios') || [])
   project_root = File.expand_path('../..', __dir__)
 
   ios_plugins.each do |p|
@@ -20,7 +33,7 @@ def flutter_install_ios_plugin_pods
       File.join(darwin_dir, "#{name}.podspec"),
       File.join(darwin_dir, "#{name}.podspec.json"),
       File.join(plugin_path,"#{name}.podspec"),
-      File.join(plugin_path,"#{name}.podspec.json")
+      File.join(plugin_path,"#{name}.podspec.json"),
     ]
 
     spec_file = candidates.find { |f| File.exist?(f) }
@@ -39,4 +52,3 @@ def flutter_install_ios_plugin_pods
       raise "No podspec for #{name} in #{plugin_path} (checked ios/, darwin/, root)"
     end
   end
-end
