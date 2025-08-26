7b747d11eef6e57d50aabaed38bd8e7d6324d6c1
Hank1583 <jouchanghung@gmail.com>
2025-08-26 13:39:09 +0800

    2025-08-26 upload 4-4

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -1,50 +1,47 @@
 #!/usr/bin/env ruby
 require 'json'
 
-def parse_KV_file(file, separator='=')
-  file_abs_path = File.expand_path(file)
-  return [] unless File.exist?(file_abs_path)
-  pods_ary = []
-  skip_line_start_symbols = ["#", "/"]
-  File.foreach(file_abs_path) do |line|
-    next if skip_line_start_symbols.any? { |symbol| line =~ /^\s*#{symbol}/ }
-    plugin = line.split(separator)
-    if plugin.length == 2
-      pods_ary.push({ name: plugin[0].strip, path: plugin[1].strip })
-    end
-  end
-  pods_ary
-end
-
 def flutter_root
-  generated = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __dir__)
-  raise "#{generated} must exist. Run `flutter pub get` first" unless File.exist?(generated)
-  File.foreach(generated) do |line|
-    m = line.match(/FLUTTER_ROOT\=(.*)/)
-    return m[1].strip if m
-  end
-  raise "FLUTTER_ROOT not found in #{generated}"
+  gen = File.expand_path(File.join('..','Flutter','Generated.xcconfig'), __dir__)
+  raise "#{gen} missing. Run `flutter pub get` first." unless File.exist?(gen)
+  File.foreach(gen) { |l| m = l.match(/FLUTTER_ROOT\=(.*)/); return m[1].strip if m }
+  raise "FLUTTER_ROOT not found in #{gen}"
 end
 
 def flutter_additional_ios_build_settings(installer)
   installer.pods_project.targets.each do |t|
-    t.build_configurations.each do |config|
-      config.build_settings['ENABLE_BITCODE'] = 'NO'
+    t.build_configurations.each do |c|
+      c.build_settings['ENABLE_BITCODE'] = 'NO'
     end
   end
 end
 
-def flutter_install_ios_plugin_pods(_ios_application_path = nil)
-  # 專案根目錄的 .flutter-plugins-dependencies
-  plugins_file = File.expand_path(File.join(__dir__, '../../.flutter-plugins-dependencies'))
-  raise "Missing #{plugins_file}. Run `flutter pub get` first." unless File.exist?(plugins_file)
+def flutter_install_ios_plugin_pods
+  # 專案根的 .flutter-plugins-dependencies
+  plugins_json = File.expand_path(File.join(__dir__, '../../.flutter-plugins-dependencies'))
+  raise "Missing #{plugins_json}. Run `flutter pub get`." unless File.exist?(plugins_json)
 
-  plugin_pods = JSON.parse(File.read(plugins_file))["plugins"]["ios"]
-  plugin_pods.each do |plugin|
-    pod plugin["name"], path: File.expand_path(plugin["path"], File.expand_path('../..', __dir__))
+  ios_plugins = JSON.parse(File.read(plugins_json)).dig("plugins","ios") || []
+  ios_plugins.each do |p|
+    plugin_name = p["name"]
+    plugin_path = File.expand_path(p["path"], File.expand_path('../..', __dir__))
+
+    # A. podspec 在套件根目錄？
+    root_podspec = File.join(plugin_path, "#{plugin_name}.podspec")
+    # B. 或在 ios 子目錄？
+    ios_dir      = File.join(plugin_path, 'ios')
+    ios_podspec  = File.join(ios_dir, "#{plugin_name}.podspec")
+
+    if File.exist?(root_podspec)
+      pod plugin_name, :path => plugin_path
+    elsif File.exist?(ios_podspec)
+      pod plugin_name, :path => ios_dir
+    else
+      raise "No podspec for #{plugin_name} under #{plugin_path} (tried root & ios/)."
+    end
   end
 end
 
-def flutter_install_all_ios_pods(_ios_application_path = nil)
+def flutter_install_all_ios_pods(_ = nil)
   flutter_install_ios_plugin_pods
 end
