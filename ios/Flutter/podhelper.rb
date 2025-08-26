c7e354cc0135cd279f05c8917f624e5b6ab3d35d
Hank1583 <jouchanghung@gmail.com>
2025-08-26 13:34:49 +0800

    2025-08-26 upload 4-3

--- ios/Flutter/podhelper.rb
+++ ios/Flutter/podhelper.rb
@@ -3,60 +3,48 @@ require 'json'
 
 def parse_KV_file(file, separator='=')
   file_abs_path = File.expand_path(file)
-  if !File.exists? file_abs_path
-    return [];
-  end
+  return [] unless File.exist?(file_abs_path)
   pods_ary = []
   skip_line_start_symbols = ["#", "/"]
   File.foreach(file_abs_path) do |line|
     next if skip_line_start_symbols.any? { |symbol| line =~ /^\s*#{symbol}/ }
-    plugin = line.split(pattern=separator)
+    plugin = line.split(separator)
     if plugin.length == 2
-      podname = plugin[0].strip()
-      path = plugin[1].strip()
-      pods_ary.push({:name => podname, :path => path});
-    else
-      puts "Invalid plugin specification: #{line}"
+      pods_ary.push({ name: plugin[0].strip, path: plugin[1].strip })
     end
   end
-  return pods_ary
+  pods_ary
 end
 
 def flutter_root
-  generated_xcode_build_settings_path = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __dir__)
-  unless File.exist?(generated_xcode_build_settings_path)
-    raise "#{generated_xcode_build_settings_path} must exist. If you're running pod install manually, make sure flutter pub get is executed first"
+  generated = File.expand_path(File.join('..', 'Flutter', 'Generated.xcconfig'), __dir__)
+  raise "#{generated} must exist. Run `flutter pub get` first" unless File.exist?(generated)
+  File.foreach(generated) do |line|
+    m = line.match(/FLUTTER_ROOT\=(.*)/)
+    return m[1].strip if m
   end
-
-  File.foreach(generated_xcode_build_settings_path) do |line|
-    matches = line.match(/FLUTTER_ROOT\=(.*)/)
-    return matches[1].strip if matches
-  end
-  raise "FLUTTER_ROOT not found in #{generated_xcode_build_settings_path}"
+  raise "FLUTTER_ROOT not found in #{generated}"
 end
 
-def flutter_ios_engine_dir
-  File.expand_path('Flutter/engine', flutter_root)
-end
-
-def flutter_additional_ios_build_settings(target)
-  # Workaround https://github.com/flutter/flutter/issues/38059
-  target.build_configurations.each do |config|
-    config.build_settings['ENABLE_BITCODE'] = 'NO'
+def flutter_additional_ios_build_settings(installer)
+  installer.pods_project.targets.each do |t|
+    t.build_configurations.each do |config|
+      config.build_settings['ENABLE_BITCODE'] = 'NO'
+    end
   end
 end
 
-def flutter_install_ios_plugin_pods(ios_application_path = nil)
-  ios_application_path ||= File.dirname(__dir__)
-  # defined in Generated.xcconfig
-  generated_xcode_build_settings_path = File.expand_path(File.join('..','Flutter','Generated.xcconfig'), __dir__)
-  plugins_file = File.join(ios_application_path, '.flutter-plugins-dependencies')
+def flutter_install_ios_plugin_pods(_ios_application_path = nil)
+  # 專案根目錄的 .flutter-plugins-dependencies
+  plugins_file = File.expand_path(File.join(__dir__, '../../.flutter-plugins-dependencies'))
+  raise "Missing #{plugins_file}. Run `flutter pub get` first." unless File.exist?(plugins_file)
+
   plugin_pods = JSON.parse(File.read(plugins_file))["plugins"]["ios"]
   plugin_pods.each do |plugin|
-    pod plugin["name"], :path => File.expand_path(plugin["path"], ios_application_path)
+    pod plugin["name"], path: File.expand_path(plugin["path"], File.expand_path('../..', __dir__))
   end
 end
 
-def flutter_install_all_ios_pods(ios_application_path = nil)
-  flutter_install_ios_plugin_pods(ios_application_path)
+def flutter_install_all_ios_pods(_ios_application_path = nil)
+  flutter_install_ios_plugin_pods
 end
